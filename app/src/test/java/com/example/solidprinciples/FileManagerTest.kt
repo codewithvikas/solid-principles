d95b28b966e99066844aac9517655db3a77cbe6b
vikas kumar jha <vikasjhag@gmail.com>
2026-05-24 09:01:46 +0530

    tests for sms notification added

--- app/src/test/java/com/example/solidprinciples/FileManagerTest.kt
+++ app/src/test/java/com/example/solidprinciples/FileManagerTest.kt
@@ -4,6 +4,7 @@ import com.example.solidprinciples.domain.srp.EmailNotification
 import com.example.solidprinciples.domain.srp.FileManager
 import com.example.solidprinciples.domain.srp.Logger
 import com.example.solidprinciples.domain.srp.NotificationService
+import com.example.solidprinciples.domain.srp.SmsNotification
 import com.example.solidprinciples.domain.srp.ThumbnailService
 import io.mockk.mockk
 import io.mockk.verify
@@ -29,6 +30,22 @@ class FileManagerTest {
         val logger = Logger()
         val notificationService: NotificationService = mockk(relaxed = true)
         val notification = EmailNotification()
+        val thumbnailService = ThumbnailService(logger, notificationService, notification)
+        val fileManager = FileManager(logger, notificationService, notification)
+        val fileName = "ChatSheet.docx"
+        val value = "Opening File $fileName"
+        fileManager.saveFile(fileName)
+        fileManager.openFile(fileName)
+        thumbnailService.generateThumbnail(fileName)
+
+        verify { notificationService.sendNotification(notification, value) }
+    }
+
+    @Test
+    fun `verify sms notification`() {
+        val logger = Logger()
+        val notificationService: NotificationService = mockk(relaxed = true)
+        val notification = SmsNotification()
         val thumbnailService = ThumbnailService(logger, notificationService, notification)
         val fileManager = FileManager(logger, notificationService, notification)
         val fileName = "ChatSheet.docx"
